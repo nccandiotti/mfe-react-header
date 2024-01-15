6696ed5e7ce7a37e2a653c0c4011d6ea01c9ff36
Nicole Candiotti <nccandiotti@gmail.com>
2024-01-14 21:21:45 -0500

    committing repo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css"
+import React from "react"
+function App() {
+  return (
+    <div className="HeaderApp">
+      <nav>
+        <ul role="menubar">
+          <li role="none">
+            <a href="#">Link 1</a>
+          </li>
+          <li role="none">
+            <a href="#">Link 2</a>
+          </li>
+          <li role="none">
+            <a href="#">Link 3</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default App
